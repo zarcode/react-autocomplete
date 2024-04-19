703cdc37f4fe8f911b4ed3e1c4ffc1ffaf5e15ec
zarcode <denciczarko@gmail.com>
2024-04-19 12:34:34 +0200

    feat: handle keyboard selection
    Select items from dropdowns with keyboard

--- src/Autocomplete/Autocomplete.tsx
+++ src/Autocomplete/Autocomplete.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Autocomplete.module.css";
 
 interface AutocompleteProps<Value> {
@@ -23,6 +23,10 @@ export default function Autocomplete<V>(props: AutocompleteProps<V>) {
   } = props;
 
   const [inputValue, setInputValue] = useState("");
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const dropdownRef = useRef(null);
+  const itemRefs = useRef<HTMLLIElement[]>([]);
 
   const derivedValue = value ? getOptionLabel(value) : "";
 
@@ -44,13 +48,55 @@ export default function Autocomplete<V>(props: AutocompleteProps<V>) {
     }
   };
 
-  const handleOptionClick =
-    (option: V) => (event: React.MouseEvent<HTMLLIElement>) => {
+  const handleOptionSelect =
+    (option: V) =>
+    (
+      event:
+        | React.MouseEvent<HTMLLIElement>
+        | React.KeyboardEvent<HTMLInputElement>
+    ) => {
       setInputValue(getOptionLabel(option));
       if (onChange) {
         onChange(event, option);
       }
     };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    switch (event.key) {
+      case "ArrowDown":
+        event.preventDefault();
+        setSelectedIndex((prev) => Math.min(prev + 1, options.length - 1));
+        break;
+      case "ArrowUp":
+        event.preventDefault();
+        setSelectedIndex((prev) => Math.max(prev - 1, 0));
+        break;
+      case "Enter":
+        event.preventDefault();
+        if (selectedIndex >= 0 && selectedIndex < options.length) {
+          handleOptionSelect(options[selectedIndex])(event);
+          setSelectedIndex(-1);
+        }
+        break;
+      case "Escape":
+        // setShowResults(false);
+        setSelectedIndex(-1);
+        break;
+    }
+  };
+
+  const handleMouseEnter = () => {
+    setSelectedIndex(-1);
+  };
+
+  useEffect(() => {
+    itemRefs.current[selectedIndex]?.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+    });
+  }, [selectedIndex]);
+
+  console.log("selectedIndex", selectedIndex);
 
   return (
     <div className={styles.autocomplete}>
@@ -60,11 +106,26 @@ export default function Autocomplete<V>(props: AutocompleteProps<V>) {
         className={styles["autocomplete-input"]}
         placeholder="Start typing..."
         onChange={handleInputChange}
+        onKeyDown={handleKeyDown}
       />
       {inputValue && (
-        <ul className={styles["autocomplete-results"]}>
+        <ul ref={dropdownRef} className={styles["autocomplete-results"]}>
           {options.map((option, index) => (
-            <li key={index} onClick={handleOptionClick(option)}>
+            <li
+              key={index}
+              className={
+                selectedIndex === index
+                  ? `${styles["autocomplete-results-item"]} ${styles.selected}`
+                  : styles["autocomplete-results-item"]
+              }
+              ref={(el) => {
+                if (el) {
+                  itemRefs.current[index] = el;
+                }
+              }}
+              onClick={handleOptionSelect(option)}
+              onMouseEnter={handleMouseEnter}
+            >
               {getOptionLabel(option)}
             </li>
           ))}
