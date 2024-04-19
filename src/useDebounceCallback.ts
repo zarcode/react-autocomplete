89007d6c3f9f34969f0df73f4431d45c85f17c60
zarcode <denciczarko@gmail.com>
2024-04-19 21:54:08 +0200

    refactor: improve debounce
    Improve useDebouncedCallback hook by persisting debounced function through re-renders

--- src/useDebounceCallback.ts
+++ src/useDebounceCallback.ts
@@ -1,20 +1,36 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
-export const useDebouncedCallback = (callback: (...args: any[]) => void, delay: number) => {
-    const timeout = useRef<ReturnType<typeof setTimeout>>();
-  
-    const debouncedFunction = useCallback(
-      (...args: any[]) => {
-        const later = () => {
-            callback(...args);
+export const useDebouncedCallback = <T extends (...args: any[]) => void>(callback: T, delay: number) => {
+    const timeoutRef = useRef<ReturnType<typeof setTimeout>>();
+    const callbackRef = useRef<T>(callback);
+
+    // Update the current callback with each render if it changes
+    useEffect(() => {
+        callbackRef.current = callback;
+    }, [callback]);
+
+
+    const debouncedFunction = useCallback((...args: any[]) => {
+        // Clears the existing timeout to ensure that it only fires once per delay period
+        if (timeoutRef.current) {
+          clearTimeout(timeoutRef.current);
+        }
+    
+        // Sets a new timeout
+        timeoutRef.current = setTimeout(() => {
+          callbackRef.current(...args);
+        }, delay);
+      }, [delay]);
+
+    // Cleanup on unmount
+    useEffect(() => {
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
         };
-  
-        clearTimeout(timeout.current);
-        timeout.current = setTimeout(later, delay);
-      },
-      [callback, delay]
-    );
+    }, []);
 
     return debouncedFunction;
   };
